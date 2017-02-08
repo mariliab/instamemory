c7eae40dd4cd05abfff4a35e38680baebb9503a7
mariliab <marilia.bognandi@gmail.com>
2017-02-08 20:23:44 +0100

    First view!

--- instamemory/ViewController.swift
+++ instamemory/ViewController.swift
@@ -10,7 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    var url1:String = "https://api.instagram.com/v1/users/"
+    var url2:String = "/media/recent/"
+    
+    
+    
+    
+
+    var difficulty: String = ""
+    
+    @IBAction func selectDifficulty(_ sender: UIButton) {
+        
+        difficulty =  "\(sender.titleLabel!.text!)"
+        print(difficulty)
+    }
+    
     override func viewDidLoad() {
+
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -18,6 +36,26 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func playGame(_ sender: UIButton) {
+        let urlString = URL(string: "\(url1)" + "\(usernameTextField!.text!)" + "\(url2)")
+        
+        if let url = urlString {
+            let task = URLSession.shared.dataTask(with: url) {
+            (data, response, error) in
+                if error != nil {
+                    print (error)
+                } else {
+                    if let usableData = data {
+                        print (usableData)
+                    }
+                }
+            }
+            task.resume()
+        }
+       
+
     }
 
 
